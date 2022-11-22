ee6f367a571172e12c371fdbfb9cf7db9d06e05c
pearl <adedijioluwaseun@yahoo.com>
2022-11-22 06:02:14 +0000

    Modifying the code

--- sign.js
+++ sign.js
@@ -1,40 +1,48 @@
+let fName = document.getElementById('fname');
+let lName = document.getElementById('lname');
+let eMail = document.getElementById('email');
+let pwd = document.getElementById('pwd');
+let form = document.querySelector('form');
+
+
 function signUp(e) {
-    //const signUp = e => {
-    let formValue = {
-        firstN : document.getElementById('fname').value,
-        lastN : document.getElementById('lname').value,
-        email : document.getElementById('email').value,
-        pass : document.getElementById('pwd').value,
-    }
+  //declaring an array to store the form values
+      let formValue = [];
+      //add values to the array
+     formValue.push({
+        firstN : fName.value,
+        lastN : lName.value,
+        email : eMail.value,
+        pass : pwd.value,
+      })
+      //save values to the local storage
     localStorage.setItem('formValue', JSON.stringify(formValue));
     disValue();
-  
+    //reset the form
+    form.reset();
+    //add the focus unto the first input
+    fName.focus()
     e.preventDefault();
 }
 
+
 function disValue(){
-  const {firstN, lastN, email, pass} = JSON.parse(localStorage.getItem('formValue'))
-  let saved = document.getElementById('saved');
-  saved.innerHTML = `
-  <table>
-  <tr>
-  <td>First Name</td>
-  <td>${firstN}</td>
-  </tr>
-
-  <tr>
-  <td>Last Name</td>
-  <td>${lastN}</td>
-  </tr>
-
-  <tr>
-  <td>Email</td>
-  <td>${email}</td>
-  </tr>
-
-  <tr>
-  <td>Password</td>
-  <td>${pass}</td>
-  </tr>
-  </table`
+  //console.log(localStorage.getItem('formValue'));
+  //check if local storage has values before rnedering
+  if (localStorage.getItem('formValue')){
+  let saved = document.querySelector('tbody');
+  //saved.innerHTML = '';
+  JSON.parse(localStorage.getItem('formValue')).forEach(element => {
+    //render onto a table in rows and colums
+     saved.innerHTML +=  `<tr>
+                      <td>${element.firstN}</td>
+                      <td>${element.lastN}</td>
+                      <td>${element.email}</td>
+                      <td>${element.pass}</td>
+                      </tr>`
+  });
 }
+}
+disValue()
+
+
